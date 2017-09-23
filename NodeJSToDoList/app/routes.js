491afb8fd5e9109a4bf811fc66dba811716ec72a
Ben <ben@bpmcgiveron.co.uk>
2017-09-23 10:17:40 +0100

    Add project files.

---
+++ NodeJSToDoList/app/routes.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+module.exports = router;
+
+router.get('/', function (req, res) {
+    res.send('hello world');
+});
+
+router.get('/about', function (req, res) {
+    res.send('this is an about page');
+});
+
+router.get('/contact');
+router.post('/contact');
+
+
---
+++ NodeJSToDoList/server.js
@@ -0,0 +1,13 @@
+'use strict';
+var express = require('express');
+var app = express();
+var port = 8080;
+
+var router = require('./app/routes');
+app.use('/', router);
+
+app.listen(port, function() {
+    console.log('app started');
+});
+
+ 
