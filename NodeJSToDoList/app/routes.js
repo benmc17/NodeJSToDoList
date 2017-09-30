f394f16d5ec89b198d051b371d44148b5aafd1fc
Ben <ben@bpmcgiveron.co.uk>
2017-09-30 10:52:18 +0100

    Split out the templates into partial files

--- NodeJSToDoList/app/routes.js
+++ NodeJSToDoList/app/routes.js
@@ -1,6 +1,5 @@
 ﻿'use strict';
 var express    = require('express');
-var path       = require('path');
 
 var router     = express.Router();
 
--- NodeJSToDoList/server.js
+++ NodeJSToDoList/server.js
@@ -1,11 +1,14 @@
 'use strict';
 var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
+var path = require('path');
+
 var app = express();
 var port = 8080;
 
 // EJS Template Engine
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayouts);
 
 // location of routes file
